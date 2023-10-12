b34757f9cc7bfab1fd12f78cceb11034e94c812d
Saupri7633 <sauravkr7250@gmail.com>
2023-10-12 16:26:30 +0530

    add all file

--- responsive.js
+++ responsive.js
@@ -1,1 +1,13 @@
 
+function showSideBar() {
+    document.getElementById("threeLine").style.display = "none";
+    document.getElementById("cross").style.display = "block";
+    document.getElementById("sld").style.display = "block";
+}
+
+function closeSideBar() {
+    document.getElementById("cross").style.display = "none";
+    document.getElementById("threeLine").style.display = "block";
+    document.getElementById("sld").style.display = "none";
+}
+
